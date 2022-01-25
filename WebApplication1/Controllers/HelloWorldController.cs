b5003875c576c165d2e593624014214fe56e1fda
Francisco Funes <ffunes90@gmail.com>
2022-01-25 13:03:17 -0300

    feat: adding all the files for the working tutorial of docs microsoft getting started with ASP .NET

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult  Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(String name, int numTimes = 1)
+        {
+            ViewBag.Message = "hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
